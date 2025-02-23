adbcafe2972f16e6dd9cdb1ab4a8f3e382328f5a
Niraj <nk5347624@gmail.com>
2025-02-23 11:40:53 +0530

    fix added db variable

--- db_Connect.js
+++ db_Connect.js
@@ -1,6 +1,6 @@
 const mongoose= require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/cental")
+mongoose.connect("mongodb+srv://nk5347624:admin@cluster0.imovo.mongodb.net/car-rental")
 .then(()=>{
     console.log("Database is Connected");
     
